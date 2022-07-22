e0aa242284dc6344300667d7c92dbd5a8480fbee
yaoruijun <yaoruijun@pwrd.com>
2022-07-22 11:54:30 +0800

    实现LPV中

--- Assets/RhythmPipeline/Runtime/LPV.cs
+++ Assets/RhythmPipeline/Runtime/LPV.cs
@@ -15,9 +15,10 @@ public class LPV
 
     
     private ScriptableRenderContext _context;
+    private Matrix4x4 _directionalShadowMatrix;
     public GameObject cube = GameObject.Find("LpvCube");
     public Vector3 size = new Vector3(100, 30, 100);
-    private static Vector3Int cellNum = new Vector3Int(2, 2, 2);
+    private static Vector3Int cellNum = new Vector3Int(8, 8, 8);
     
     private LpvCell[,,] lpvCells = new LpvCell[cellNum.x,cellNum.y,cellNum.z];
     
@@ -40,9 +41,36 @@ public class LPV
     public ComputeShader lightInject;
     public static int injectKernel;
 
-    private  RenderTexture lpvTex;
+    private RenderTexture lpvTexR;
+    private RenderTexture lpvTexG;
+    private RenderTexture lpvTexB;
     // LightInject, 使用Compute Shader, 每个Cell一个线程，采样cell内的RSM，把结果投影到SH。
     private void LightInject()
+    {
+        InitLpvTex(ref lpvTexR);
+        InitLpvTex(ref lpvTexG);
+        InitLpvTex(ref lpvTexB);
+        
+        lightInject = AssetDatabase.LoadAssetAtPath<ComputeShader>("Assets/RhythmPipeline/Shaders/GI/LightInject.compute");
+        injectKernel = lightInject.FindKernel("LIGHT_INJECT");
+        
+        var cmd = CommandBufferPool.Get(bufferName);
+        cmd.BeginSample("Light Inject");
+        cmd.SetComputeVectorParam(lightInject, "AABB_MIN", _lpvBox.bounds.min);
+        cmd.SetComputeVectorParam(lightInject, "CELL_SIZE", LpvCell.size);
+        // cmd.SetComputeMatrixParam(lightInject, "DIRECRTIONAL_SHADOW_MATRIX", Matrix4x4.identity);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "_WorldPos", RSM._rsmTargets[0]);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "_Flux", RSM._rsmTargets[1]);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "_WorldNormal", RSM._rsmTargets[2]);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "LpvTexR", lpvTexR);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "LpvTexG", lpvTexG);
+        cmd.SetComputeTextureParam(lightInject, injectKernel, "LpvTexB", lpvTexB);
+        cmd.DispatchCompute(lightInject, injectKernel, cellNum.x, cellNum.y, cellNum.z);
+        cmd.EndSample("Light Inject");
+        CommandHelper.ExecuteAndRelese(_context, cmd);
+    }
+
+    private void InitLpvTex(ref RenderTexture lpvTex)
     {
         if (lpvTex == null)
         {
@@ -51,28 +79,15 @@ public class LPV
             rtDescriptor.width = cellNum.x;
             rtDescriptor.height = cellNum.y;
             rtDescriptor.volumeDepth = cellNum.z;
-            rtDescriptor.graphicsFormat = GraphicsFormat.R8G8B8_UNorm;
+            rtDescriptor.graphicsFormat = GraphicsFormat.R16G16B16_UNorm;
             rtDescriptor.colorFormat = RenderTextureFormat.Default;
             rtDescriptor.msaaSamples = 1;
             lpvTex = new RenderTexture(rtDescriptor);
         }
-        
         lpvTex.enableRandomWrite = true;
         if (!lpvTex.IsCreated()) lpvTex.Create();
-        
-        lightInject = AssetDatabase.LoadAssetAtPath<ComputeShader>("Assets/RhythmPipeline/Shaders/GI/LightInject.compute");
-        injectKernel = lightInject.FindKernel("LIGHT_INJECT");
-        
-        var cmd = CommandBufferPool.Get(bufferName);
-        cmd.BeginSample("LightInject");
-        cmd.SetComputeTextureParam(lightInject, injectKernel, "_WorldPos", RSM._rsmTargets[0]);
-        cmd.SetComputeTextureParam(lightInject, injectKernel, "_Flux", RSM._rsmTargets[1]);
-        cmd.SetComputeTextureParam(lightInject, injectKernel, "_WorldNormal", RSM._rsmTargets[2]);
-        cmd.SetComputeTextureParam(lightInject, injectKernel, "LpvTex", lpvTex);
-        cmd.DispatchCompute(lightInject, injectKernel, cellNum.x, cellNum.y, cellNum.z);
-        cmd.EndSample("LightInject");
-        CommandHelper.ExecuteAndRelese(_context, cmd);
     }
+    
     private Vector3 VecMult(Vector3 v1, Vector3 v2)
     {
         return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
@@ -82,12 +97,12 @@ public class LPV
         return new Vector3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
     }
 
-    
+    private BoxCollider _lpvBox;
     private void Init()
     {
-        var lpvBox = RhythmPipeline.PipelineManager.RhythmPipeline.LPVBOX.GetComponent<BoxCollider>();
-        size = lpvBox.size;
-        var aabbMin = lpvBox.bounds.min;
+        _lpvBox = RhythmPipeline.PipelineManager.RhythmPipeline.LPVBOX.GetComponent<BoxCollider>();
+        size = _lpvBox.size;
+        var aabbMin = _lpvBox.bounds.min;
         
         LpvCell.size = VecDivide(size, cellNum);
         cube.transform.localScale = LpvCell.size;
